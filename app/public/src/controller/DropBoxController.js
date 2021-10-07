22d626805f0cc87293b09f69f2016b75bcff111c
Lucianoal0511 <lucianoal0511@gmail.com>
2021-10-07 00:44:13 -0300

    iniciando o projeto

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -5,6 +5,9 @@ class DropBoxController {
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
+        this.namefileEl = this.snackModalEl.querySelector('.filename')
+        this.timeleftEl = this.snackModalEl.querySelector('.timeleft')
 
         this.initEvents();
     }
@@ -19,16 +22,116 @@ class DropBoxController {
 
         this.inputFilesEl.addEventListener('change', event => {
 
-            console.log(event.target.files)
+            //console.log(event.target.files);
+            this.uploadTask(event.target.files);
 
-            this.snackModalEl.style.display = 'block';
+            this.modalShow();
+
+            this.inputFilesEl.value = '';//Aqui zero a barra de progresso
+
+            //this.snackModalEl.style.display = 'block';
             
         });
     }
 
+    //método para esconder o modal
+    modalShow(show = true){
+
+        this.snackModalEl.style.display = (show) ? 'block' : 'none';
+
+    }
+
     uploadTask(files){
 
-        let promise
+        let promises = [];
+
+        //convertendo o file em array (usando spread)
+        [...files].forEach(file => {
+
+            promises.push(new Promise((resolve, reject) => {
+                
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+
+                    this.modalShow(false);
+
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e) {
+                        reject(e);
+                    }
+                };
+
+                ajax.onerror = event => {
+
+                    this.modalShow(false);
+                    reject(event);
+
+                };
+
+                //Para a barra de progresso
+                ajax.upload.onprogress = event => {
+
+                    this.uploadProgress(event, file);
+                    //console.log(event);
+
+                }
+
+                let formData = new FormData();
+                formData.append('input-file', file);
+
+                this.startUploadTime = Date.now();//Pegar o tempo exato que iniciou o upload
+
+                ajax.send(formData);
+
+            }));
+            
+        })
+
+        return Promise.all(promises);
+    }
+
+    uploadProgress(event, file){
+
+        let timeSpent = Date.now() - this.startUploadTime;
+        let loaded = event.loaded;
+        let total = event.total;
+        let porcent = parseInt((loaded/total) * 100);//Cálculo em inteiros
+        let timeleft = ((100 - porcent) * timeSpent) / porcent;
+
+        this.progressBarEl.style.width = `${porcent}%`;//atualizando a progressão da barra
+
+        this.namefileEl.innerHTML = file.name;//atualizando o nome do arquivo
+        this.timeleftEl.innerHTML = this.formatTimeToHuman(timeleft);//atualizando o tempo que falta
+
+        //console.log(timeSpent, timeleft, porcent);
+
+    }
+
+    //Método para formatar o tempo
+    formatTimeToHuman(duration){
+
+        let seconds = parseInt(duration / 1000) % 60;
+        let minutes = parseInt(duration / (1000 * 60)) % 60;
+        let hours = parseInt(duration / (1000 * 60 * 60)) % 24;
+
+        if (hours > 0) {
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+        }
+
+        if (minutes > 0) {
+            return `${minutes} minutos e ${seconds} segundos`;
+        }
+
+        if (seconds > 0) {
+            return `${seconds} segundos`;
+        }
+
+        return '';
+
     }
 
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res) => {
+
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',//foi criado um diretório para colocar os arquivos
+    keepExtensions: true //manter a extensão do arquivo
+  });
+
+  form.parse(req, (err, fields, files) => {
+
+    res.json({
+      files //files: files pode ser assim também
+    });
+
+  });
+
 });
 
 module.exports = router;
